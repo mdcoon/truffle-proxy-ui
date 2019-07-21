26541619d24ffcc9dc956d19353893629b76a1fc
Mitchell Opatowsky <opatowsky77@gmail.com>
2019-07-21 10:43:14 -0400

    button changes

--- src/Views/main/interactions/Interactions.jsx
+++ src/Views/main/interactions/Interactions.jsx
@@ -2,27 +2,32 @@ import React from "react";
 import { Card, Button, CardTitle, CardText, Row, Col } from "reactstrap";
 import classNames from "classnames";
 import * as align from "Constants/alignments";
+import styles from "./interactions.module.scss";
 
 const Interactions = props => {
   return (
     <div className={classNames(["text-center"], ["container"], ["mt-5"])}>
       <p>
-        Use these functions to interact with Metamask to simulate proxy contract
-        deployment.
+        Use these functions to interact with Metamask to interact with the
+        deployed proxy contract.
       </p>
       <Row>
         <Col sm="6">
           <Card body>
             <CardTitle>Visit</CardTitle>
-            <CardText>Here’s a visit description here.</CardText>
-            <Button onClick={props.contractVisit}>Go somewhere</Button>
+            <CardText>Sign the guestbook.</CardText>
+            <Button color="info" onClick={props.contractVisit}>
+              Visit
+            </Button>
           </Card>
         </Col>
         <Col sm="6">
           <Card body>
             <CardTitle>Increment</CardTitle>
-            <CardText>Here’s an increment description here</CardText>
-            <Button onClick={props.contractIncrement}>Increment</Button>
+            <CardText>Increment account.</CardText>
+            <Button color="success" onClick={props.contractIncrement}>
+              Increment
+            </Button>
           </Card>
         </Col>
       </Row>
